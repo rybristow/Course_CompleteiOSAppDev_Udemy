bfa9fb99b0d30049ee7e0fd16b118a2066b7aba0
Ryland Bristow <rylandb@g.clemson.edu>
2022-01-21 23:34:24 -0500

    feature: added some questions to cycle through as well as a score tracker

--- Projects/Quizzler-LayoutPractice-iOS13-master/Quizzler-iOS13/ViewController.swift
+++ Projects/Quizzler-LayoutPractice-iOS13-master/Quizzler-iOS13/ViewController.swift
@@ -16,14 +16,45 @@ class ViewController: UIViewController {
   @IBOutlet weak var trueButton: UIButton!
   @IBOutlet weak var falseButton: UIButton!
   
+  struct Question {
+    var title: String
+    var answer: String
+  }
+  
+  let quiz = [
+    Question(title:"4 + 2 = 6", answer: "True"),
+    Question(title:"5 - 3 > 1", answer: "True"),
+    Question(title:"3 - 1 < 1", answer: "False")
+  ]
+  var questionNumber = 0
+  
+  var score = 0
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
-    questionLabel.text = "4 + 2 = 6"
+    questionLabel.text = quiz[questionNumber][0]
   }
   
   @IBAction func answerButtonPressed(_ sender: UIButton) {
+    if(sender.currentTitle == quiz[questionNumber][1]) {
+        score += 1
+        print("Correct!")
+        print("Score: \(score)")
+      }
+      else {
+        print("Incorrect :(")
+      }
     
+    if(questionNumber + 1 < quiz.count) {
+      questionNumber += 1
+    }
+    else {
+      questionNumber = 0
+    }
+    
+    questionLabel.text = quiz[questionNumber][0]
   }
   
 }
